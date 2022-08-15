f84c16703e48f7c8713ce8cb9e211fb16344abeb
Shakhzod-Xojamberdiyev <shakhzod6362@gmail.com>
2022-08-15 10:56:06 +0500

    Add new

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var elBurgerBtn = document.querySelector(".header__burger");
+elBurgerBtn.addEventListener("click" , function(){
+    elBurgerBtn.closest(".header").classList.toggle("header__nav-open")
+})
